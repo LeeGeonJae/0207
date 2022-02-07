74038201bbefbc41c0c7be2cfbf9a2f909e62487
LeeeGeonJae <wnwkrdkemf@gmail.com>
2022-02-07 10:18:51 +0900

    Engine 설정 World 설정

--- MyEngine.h
+++ MyEngine.h
@@ -1,7 +1,30 @@
 #pragma once
+
+class World;
+
 class MyEngine
 {
 public:
+	MyEngine();
+	virtual ~MyEngine();
 
+	void Run();
+
+	void Stop();
+	
+	void SpawnActor(class Actor* NewActor);
+	void DestroyActor(class Actor* DestroyActor);
+
+protected:
+	void BeginPlay();
+	void Tick();
+	void Render();
+	void Input();
+
+protected:
+	bool bIsRunning = true;
+
+	//포인터로 메모리에 범위를 지정한다.
+	//앞에 class World;를 입력하면 앞에 class를 쓰지 않아도 된다.
+	class World* CurrentWorld;
 };
-
