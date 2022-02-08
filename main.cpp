18fccffd76aa1d8cbee50c121ebb07dc567432cd
LeeeGeonJae <wnwkrdkemf@gmail.com>
2022-02-08 13:19:39 +0900

    윈도우 생성한 창에 빨간색 선 긋기 'q' 누르면 꺼지기

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@ using namespace std;
 
 int SDL_main(int argc, char* argv[])
 {
-	/*if ( SDL_Init(SDL_INIT_VIDEO) < 0 );
-	{
-		cout << "SDL_Init Error :" << SDL_GetError() << endl;
-		return -1;
-	}*/
+
+	//if ( SDL_Init(SDL_INIT_VIDEO) < 0 );
+	//{
+	//	cout << "SDL_Init Error :" << SDL_GetError() << endl;
+	//	return -1;
+	//}
 
 	SDL_Window* MyWindow = SDL_CreateWindow("First SDL Example", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+
+	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED |
+		SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+
+	if (MyRenderer == nullptr)
+	{
+		cout << "can't Create renderer :" << SDL_GetError() << endl;
+		return -1;
+	}
 
 	SDL_Event MyEvent;
 
@@ -25,6 +35,7 @@ int SDL_main(int argc, char* argv[])
 
 	while (bIsRunning)
 	{
+		//Input
 		SDL_PollEvent(&MyEvent);
 
 		switch (MyEvent.type)
@@ -34,15 +45,36 @@ int SDL_main(int argc, char* argv[])
 			break;
 		case SDL_KEYDOWN:
 			cout << SDL_GetKeyName(MyEvent.key.keysym.sym) << "키 눌러짐" << endl;
+			switch (MyEvent.key.keysym.sym)
+			{
+			case SDLK_q:
+				bIsRunning = false;
+				break;
+			}
 			break;
 		case SDL_MOUSEBUTTONDOWN:
 			cout << (MyEvent.button.button == SDL_BUTTON_LEFT) << "마우스 버튼 눌러짐" << endl;
 			cout << "(" << MyEvent.button.x << ", " << MyEvent.button.y << ")" << endl;
 			break;
 		}
+
+
+		//PreRender(그릴 준비, 그릴 물체 배치)
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0xff, 0xff);
+		SDL_RenderClear(MyRenderer);
+
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0, 0, 0);
+		SDL_RenderDrawLine(MyRenderer, 100, 100, 200, 200);
+
+		//GPU야 그려라
+		//Render
+		SDL_RenderPresent(MyRenderer);
+
 	}
 
+	
 
+	SDL_DestroyRenderer(MyRenderer);
 	SDL_DestroyWindow(MyWindow);
 	SDL_Quit();
 
